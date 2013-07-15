2635950791c9d801f97396060c35dbf680be066a
Tully Foote <tfoote@willowgarage.com>
2013-07-15 12:54:21 -0700

    creating separate branch for demo app from library

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,9 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := rosbag_ndk_player
-LOCAL_SRC_FILES := src/replay.cpp src/bag_player.cpp
-#LOCAL_SRC_FILES := src/record.cpp
+LOCAL_MODULE    := rosbag_ndk_player_demo
+LOCAL_SRC_FILES := src/test.cpp
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_LDLIBS := -landroid
 LOCAL_STATIC_LIBRARIES := android_native_app_glue rosbag
@@ -12,4 +11,4 @@ LOCAL_STATIC_LIBRARIES := android_native_app_glue rosbag
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/native_app_glue)
-$(call import-module,rosbag_ndk)
+$(call import-module,rosbag_ndk_player)
